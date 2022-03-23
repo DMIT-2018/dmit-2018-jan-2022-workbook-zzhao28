c8775986241e37b7406d1e1aaf7280568ba14597
Zeyu Zhao <zzhao28@nait.ca>
2022-03-23 08:33:03 -0600

    Complete Data Models

---
+++ src/ChinookSolution/ChinookSystem/ViewModels/PlaylistMove.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.ViewModels
+{
+    public class PlaylistMove
+    {
+        public int TrackId { get; set; }
+        public bool SelectedTrack { get; set; }
+        public string TrackInput { get; set; }
+
+    }
+}
